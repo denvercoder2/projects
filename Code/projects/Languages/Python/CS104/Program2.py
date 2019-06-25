b3ec50198299ff01d350902c6906dc426028e289
River Holley <riverholley0923@gmail.com>
2019-06-25 11:13:13 -0500

    Added file for program 2

---
+++ Code/projects/Languages/Python/CS104/Program2.py
@@ -0,0 +1,9 @@
+# --------------- Introductory --------------#
+# Scott Holley
+# Program 2, CS104
+# Date: June 25th, 2019
+# Run and Tested on Python 3.7.3 Anaconda
+# Purpose: To compute invoices based on the user input
+# --------------- Introductory --------------#
+
+# --------------- Start of Program --------------#
