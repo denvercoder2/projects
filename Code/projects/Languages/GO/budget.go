9af68bfa26493b9e5cb135072a21259cc0d2eac1
River Holley <riverholley0923@gmail.com>
2019-08-02 17:16:05 -0500

    Finished budget program in GO

--- Code/projects/Languages/GO/budget.go
+++ Code/projects/Languages/GO/budget.go
@@ -1,48 +1,134 @@
-// /*
-// GO implementation of budget
-// */
+/*
+GO implementation of budget
+Scott
+*/
 
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// )
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
-// func readBills(utility float64) map[string]float64 {
-// 	var newUtility float64
+/*
+* Function Name: readBills
+* Parameters: Utility bill (float64)
+* Return Type: map of string keys, float64 values
 
-// 	newUtility = math.Pow(utility, .5)
-// 	billMap := map[string]float64{
-// 		"Rent":        410.555,
-// 		"Water":       20.00,
-// 		"Internet":    55.00,
-// 		"Utilities":   newUtility,
-// 		"Phone":       50.00,
-// 		"Insurance":   160.00,
-// 		"Car Payment": 221.14}
+* Purpose: Returns a mapping of bills to be used in
+	   scope for other functions and mitigate
+	   unwanted side effects
+*/
+func readBills(utility float64) map[string]float64 {
 
-// 	return billMap
-// }
+	var newUtility float64
 
-// func getSaving(total float64) float64 {
-// 	var savings float64
+	newUtility = math.Pow(utility, .5)
+	billMap := map[string]float64{
+		"Rent":        410.555,
+		"Water":       20.00,
+		"Internet":    55.00,
+		"Utilities":   newUtility,
+		"Phone":       50.00,
+		"Insurance":   160.00,
+		"Car Payment": 221.14}
 
-// 	savings = total * .35
+	return billMap
+}
 
-// 	return savings
-// }
+/*
+* Function Name: sortBills
+* Parameters: map of string keys, float64 values
+* Return Type: map of string keys, float64 values
 
-// func main() {
-// 	var utility float64
-// 	var pay float64
-// 	pay = 2280.00
+* Purpose: Given a map of bills, this function
+           will	take the values and return
+	   a sorted array slice of the values
+*/
+func sortBills(map[string]float64) []float64 {
 
-// 	fmt.Print("What was the utility bill? \n")
-// 	fmt.Scan(&utility)
+	var givenMap map[string]float64
+	var onlyNums []float64
 
-// 	newMap := readBills(utility)
-// 	Rent := newMap["Rent"].(float64)
+	givenMap = readBills(0.00)
+	keys := make([]string, 0, len(givenMap))
 
-// 	fmt.Print(newMap)
-// }
+	for k := range givenMap {
+		keys = append(keys, k)
+	}
+
+	for _, k := range keys {
+		onlyNums = append(onlyNums, givenMap[k])
+	}
+	sort.Float64s(onlyNums)
+
+	return onlyNums
+}
+
+/*
+* Function Name: getSaving
+* Parameters: Total pay(float64)
+* Return Type: savings (float64)
+
+* Purpose: Takes a total and returns the
+	   appropriate saving amount from
+	   that total
+*/
+
+func getSaving(total float64) float64 {
+	var savings float64
+	savings = total * .35
+
+	return savings
+}
+
+/*
+Function Name: sumBills
+Parameters: array of bills ([]float64)
+Return Type: sum (float64)
+
+Purpose: Take the bill array and return
+	 a sum of all its elements
+*/
+func sumElements([]float64) float64 {
+	//var givenMap map[string]float64
+	var sortedList []float64
+	var sum float64
+	givenMap = readBills(0.00)
+	sortedList = sortBills(givenMap)
+
+	for i := 0; i < len(sortedList); i++ {
+		sum = sum + sortedList[i]
+	}
+
+	return sum
+}
+
+/*
+* Function Name: Main
+* Parameters: None
+* Return Type: None
+
+* Purpose: Setup and run this program
+ */
+func main() {
+	var utility float64
+	var pay float64
+	var netPay float64
+	pay = 2280.00
+
+	fmt.Print("What was the utility bill? \n")
+	fmt.Scan(&utility)
+
+	newMap := readBills(utility)
+	sortedMap := sortBills(newMap)
+
+	fmt.Print("Bills from least to most expensive: ", sortedMap, "\n")
+	totalBills := sumElements(sortedMap)
+	fmt.Print("\nTotal Bill value is: $", totalBills)
+	netPay = pay - totalBills
+	fmt.Print("\nMoney after Bills: $", netPay)
+	savings := getSaving(netPay)
+	fmt.Print("\nYou should save: $", savings)
+}
