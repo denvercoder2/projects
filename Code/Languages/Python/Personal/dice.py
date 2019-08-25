cc42e23fd88b1addc2a3f56ee814924c33da2c6a
River Holley <denvercoder2@gmail.com>
2019-08-25 00:25:41 -0500

    Added a comment

--- Code/Languages/Python/Personal/dice.py
+++ Code/Languages/Python/Personal/dice.py
@@ -1,6 +1,9 @@
 # dice
 import random
 def dice():
+        '''
+        Function will generate random number 50 times
+        '''
     for i in range(1,51):
         x = random.randrange(1, 7)
         print ("Outcome on roll: ", i, "is ---->", x)
