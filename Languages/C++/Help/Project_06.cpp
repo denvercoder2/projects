797a11e170906ba9ed403d52d07d641b9adb0dec
River Holley <denvercoder2@gmail.com>
2019-10-05 18:57:57 -0500

    Added space for Project2_470

--- Languages/C++/Help/Project_06.cpp
+++ Languages/C++/Help/Project_06.cpp
@@ -5,6 +5,26 @@
 // Course Section: CPE-211-01
 // Lab Section: 3
 // Due Date: 10/04/2019
+// Program Description: Read in the scores from the file
+// and pass the totals back out to an output file
+// on the input file
+//***************************************
+
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// command line argument requirement
+//***************************************
+// Program Title: Score Calculator
+// Project File: Project_06.cpp
+// Name: Ivan Fontane
+// Course Section: CPE-211-01
+// Lab Section: 3
+// Due Date: 10/04/2019
 // Program Description: STUFF
 // on the input file
 //***************************************
@@ -19,16 +39,18 @@ using namespace std;
 // currently will open file if possible and read lines
 int main(int argc, char* argv[])
 {
-    if (argc < 2){
+    if (argc != 3){
         cout << "You didn't give enough arguments" << endl;
         exit(1);
     }
     
-    for (int i = 1; i < argc; i++){ // i=1, assuming files arguments are right after the executable
-        string fn = argv[i]; //filename
+    for (int i = 1; i < argc; i++){ 
+        string fn_in = argv[i]; //filename input
+        string fn_out = argv[2];
         // cout << fn;
         fstream infile;
-        infile.open(fn);
+        fstream outfile;
+        infile.open(fn_in);
         if (infile.is_open()){
             string line1;
             getline(infile, line1);
@@ -45,7 +67,28 @@ int main(int argc, char* argv[])
             string line4;
             getline(infile, line4);
             cout << "\n" << line4 << endl;
+        
+
+    // check that file exists and can open
+    if(!infile){
+        cout<<string(15,'*')<<" File Open Error"<<string(15,'*')<<endl;
+        cout<<"==> Input file failed to open properly!!\n";
+        cout<<"==> Attempted to open file: "<< fn_in <<endl;
+        cout<<"==> Terminating program!!!\n";
+        cout<<string(47,'*')<<endl;
+    }
+    // error check for if file can't open 
+    if(!outfile){
+    outfile.clear();
+    cout<<string(15,'*')<<" File Open Error"<<string(15,'*')<<endl;
+    cout<<"==> Output file failed to open prperly!!\n";
+    cout<<"==> Attempted to open file: "<< fn_out <<endl;
+    cout<<"==> Terminating program!!!\n";
+    cout<<string(47,'*')<<endl;
         }
+
         infile.close();
+        outfile.close();
     }
 }
+
