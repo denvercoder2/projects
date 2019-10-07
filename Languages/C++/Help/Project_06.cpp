36b5d2852e792f44f90f05429b61b8b2d241ac88
River Holley <denvercoder2@gmail.com>
2019-10-06 22:00:32 -0500

    Help progress

--- Languages/C++/Help/Project_06.cpp
+++ Languages/C++/Help/Project_06.cpp
@@ -1,94 +1,138 @@
-//***************************************
-// Program Title: Score Calculator
+// ****************************************
+// Program Title: Project_06
 // Project File: Project_06.cpp
-// Name: Ivan Fontane
-// Course Section: CPE-211-01
-// Lab Section: 3
-// Due Date: 10/04/2019
-// Program Description: Read in the scores from the file
-// and pass the totals back out to an output file
-// on the input file
-//***************************************
-
-#include <iomanip>
-#include <fstream>
-#include <string>
+// Name: Ivan Fontane 
+// Course Section: CPE-211-01 
+// Lab Section: 4 
+// Due Date: 10/06/16 
+// Program Description:
+// ****************************************
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
 
 using namespace std;
+int main(int argc, char *argv[])
+	{
+		ifstream InFile;
+		ofstream OutFile;
 
-// command line argument requirement
-//***************************************
-// Program Title: Score Calculator
-// Project File: Project_06.cpp
-// Name: Ivan Fontane
-// Course Section: CPE-211-01
-// Lab Section: 3
-// Due Date: 10/04/2019
-// Program Description: STUFF
-// on the input file
-//***************************************
+		string command = argv[0];
+		string file = argv[1];
+		string output = argv[2];
 
-#include <iomanip>
-#include <fstream>
-#include <string>
-#include <iostream>
+		int numargs = argc;
+		if(numargs != 3)
+		{	
+			cout << "Incorrect number of command line arguments provided." << endl;
+			cout << "This program requires 2 command line arguments:" << endl;
+			cout << "An input filename and an output filename:" << endl;
+			cout << " " << endl;
+			cout << "Program usage is:" << endl;
+			cout << "./Project_06 InputFileName OutputFileName" << endl;
+			return 0;
+		}
+		
+		InFile.open(file.c_str());
 
-using namespace std;
+		cout << " " << endl;
+		cout << "Opening Input File: " << file << endl;
+		cout << " " << endl;
 
-// currently will open file if possible and read lines
-int main(int argc, char* argv[])
-{
-    if (argc != 3){
-        cout << "You didn't give enough arguments" << endl;
-        exit(1);
-    }
-    
-    for (int i = 1; i < argc; i++){ 
-        string fn_in = argv[i]; //filename input
-        string fn_out = argv[2];
-        // cout << fn;
-        fstream infile;
-        fstream outfile;
-        infile.open(fn_in);
-        if (infile.is_open()){
-            string line1;
-            getline(infile, line1);
-            cout << "\n\n" << line1 << endl;
+		while(InFile.fail())
+		{
+			cout << "*************** File Open Error ***************" << endl;
+			cout << "==> Input file failed to open properly!!" << endl;
+			cout << "==> Attempted to open file: " << file << endl;	
+			cout << "==> Please try again..." << endl;
+			cout << "***********************************************" << endl;
+			cout << " " << endl;				
+			cout << "Enter the name of the input file: ";
+			cin >> file;
+			cout << file << endl;
+			cout << " " << endl;
+			InFile.open(file.c_str());
+		}
+		
+		OutFile.open(output.c_str());
+		
+		cout << " " << endl;
+		cout << "Opening Output File: " << output << endl;
+		cout << " " << endl;
 
-            string line2;
-            getline(infile, line2);
-            cout << "\n" << line2 << endl;
+		while(OutFile.fail())
+		{
+			cout << "*************** File Open Error ***************" << endl;
+			cout << "==> Output file failed to open properly!!" << endl;
+			cout << "==> Attempted to open file: " << file << endl;	
+			cout << "==> Please try again..." << endl;
+			cout << "***********************************************" << endl;
+			cout << " " << endl;				
+			cout << "Enter the name of the Output file: ";
+			cin >> output;
+			cout << output << endl;
+			cout << " " << endl;
+			OutFile.open(output.c_str());
+		}
 
-            string line3;
-            getline(infile, line3);
-            cout << "\n" << line3 << endl;
+		int quiznum;
+		int hwnum;
+		int examnum;
+		int average;
+		
+		InFile >> quiznum;
+		InFile >> hwnum;
+		InFile >> examnum;
+		
+		string Key1;
+		string Key2;
+		int KeyQuiz;
+		int KeyHW;
+		int KeyExam;
+		int KeyTotal;
+		double KeyAverage;
 
-            string line4;
-            getline(infile, line4);
-            cout << "\n" << line4 << endl;
-        
+		InFile >> Key1;
+		InFile >> Key2;
 
-    // check that file exists and can open
-    if(!infile){
-        cout<<string(15,'*')<<" File Open Error"<<string(15,'*')<<endl;
-        cout<<"==> Input file failed to open properly!!\n";
-        cout<<"==> Attempted to open file: "<< fn_in <<endl;
-        cout<<"==> Terminating program!!!\n";
-        cout<<string(47,'*')<<endl;
-    }
-    // error check for if file can't open 
-    if(!outfile){
-    outfile.clear();
-    cout<<string(15,'*')<<" File Open Error"<<string(15,'*')<<endl;
-    cout<<"==> Output file failed to open prperly!!\n";
-    cout<<"==> Attempted to open file: "<< fn_out <<endl;
-    cout<<"==> Terminating program!!!\n";
-    cout<<string(47,'*')<<endl;
-        }
+		int i;
+		int x;
 
-        infile.close();
-        outfile.close();
-    }
-}
+		for(i = 0; i < quiznum; i++)
+		{
+			InFile >> x;
+			KeyQuiz = x + x;
+		}		
+		
+		for(i = 0; i < hwnum; i++)
+		{
+			InFile >> x;
+			KeyHW = x + x;
+		}
+		
+		for(i = 0; i < examnum; i++)
+		{
+			InFile >> x;
+			KeyExam = x + x;
 
+		}
+		cout << "KeyQuiz score: " << KeyQuiz << endl;
+		cout << "KeyHW score: " <<KeyHW << endl;
+		cout << "KeyExam score: " << KeyExam << endl;	
+
+
+		KeyTotal = KeyQuiz + KeyExam + KeyHW;
+		KeyAverage = KeyTotal / 3;
+
+		
+		
+		OutFile << left << setw(3) << "#" << setw(12) << "Last" << setw(7) << "First" 
+		<< setw(6) << "Quiz" << setw(6) << "HW" << setw(6) << "Exam" << setw(7) << "Total" << setw(9) << "Average" << endl;
+		OutFile << left << setw(3) << Key1 << setw(12) << "          " << setw(7) << Key2
+		<< setw(6) << quiznum << setw(6) << hwnum << setw(6) << examnum << setw(7) << KeyTotal << setw(9) << KeyAverage << endl;
+		
+		
+
+	 return 0;
+	}
