b70676f33a3a072264a0cbd08d2088f749d182c8
River Holley <riverholley0923@gmail.com>
2019-10-14 10:18:36 -0500

    More work on Project2_470

--- Languages/Java/Project2_470/MobileDevice.java
+++ Languages/Java/Project2_470/MobileDevice.java
@@ -4,11 +4,28 @@
     Due: November 4th
     Filename: MobileDevice
  */
+
+
+/*
+======================================== GENERAL OVERVIEW ========================================
+| The mobile device class should be able to recognize the projector                              |
+| by name (display:projector)                                                                    |
+|                                                                                                |
+| Depending on the projector (Either classroom or dorm), there can                               |
+| be a certain type of media sent to the projector (UDP packets)                                 |
+| In this project, we are to mimic live sports being sent to the projector                       |
+======================================== GENERAL OVERVIEW ========================================
+*/
 import java.net.*;
 import java.io.*; 
-  
+
+// The mobile device acts like a client
 public class MobileDevice{
 
+    private Socket socket            = null; 
+    private BufferedReader   input   = null; 
+    private DataOutputStream output  = null; 
+  
 
     public static void main(String[] args) {
         System.out.println("TBD");
--- Languages/Java/Project2_470/Projector.java
+++ Languages/Java/Project2_470/Projector.java
@@ -8,12 +8,36 @@ import java.net.*;
 import java.io.*; 
   
 
+/*
+======================================== GENERAL OVERVIEW ========================================
+| The Projector Class is to act as an interface for the mobile device to find                    |
+| The projector should have two modes (Classroom, or dorm).                                      |
+| This is to be specified to the mobile device, and from there, the type of media                |
+| that can be displayed is determined.                                                           |
+======================================== GENERAL OVERVIEW ========================================
+*/
+import java.net.*;
+
+// The projector acts like the server
 public class Projector{
+    // TCP connection initilization
+    private Socket socket           = null; 
+    private ServerSocket projector  = null; 
+    private DataInputStream input   = null; 
 
     // constructor
-    public static class ProjectorClass{
-        private String identifier;
+    public Projector(int portNum){
+        try {
+            projector = new ServerSocket(portNum); 
+            System.out.println("Server up and waiting for client..."); 
+            // Accept the connection from client and grab the ip via parameter
+            socket = server.accept(); 
+            String IPaddress = socket.getRemoteSocketAddress().toString();
+            System.out.println("Client accepted at IP and port: " + IPaddress); 
 
+        } catch (Exception e) {
+            System.out.println("Connection to projector could not be established");
+        }
     }
 
 
