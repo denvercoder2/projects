c101e3a7899dd5efbad486639b7a74158f2384f3
River Holley <denvercoder2@gmail.com>
2019-10-27 21:36:09 -0500

    Fixed import

--- Languages/Java/Project2_470/Server.java
+++ Languages/Java/Project2_470/Server.java
@@ -7,11 +7,12 @@
 */
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 /*
 ======================================== GENERAL OVERVIEW ========================================
-| The Server Class is to act as an interface for the mobile device to find                    |
-| The Server should have two modes (Classroom, or dorm).                                      |
+| The Server Class is to act as an interface for the mobile device to find                       |
+| The Server should have two modes (Classroom, or dorm).                                         |
 | This is to be specified to the mobile device, and from there, the type of media                |
 | that can be displayed is determined.                                                           |
 | There is also a choice on what type of cast to use                                             |
@@ -27,7 +28,7 @@ import java.net.*;
 
 public class Server {
 
-   
+
 
 
 }
