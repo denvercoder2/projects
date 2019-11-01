7e3b77b936360237a7948bb0ea640b693025813a
River Holley <denvercoder2@gmail.com>
2019-11-01 00:11:12 -0500

    Major work on 490 project

--- Languages/C++/490/Round_Robbin.cpp
+++ Languages/C++/490/Round_Robbin.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include<string>
+#include <fstream>
+#include <sstream> 
+#include <string>
 
 /*
 =================== Program Overview =================== 
-*   Implement the Round Robbin algorithm in C++
+*    Implement the Round Robbin algorithm in C++
 *    While calculating and displaying individual
 *    and System statistics as mentioned in program
 *    requirements. They are as follows:
 -------------------------------------------------------------
 *    Individual: classification of process.
-*    - short (finishes within the first time quantum), 
+*    - short (finishes within the first time q), 
 *    - medium (finishes within two quanta)
 *    - long (requires more than two quanta to complete);  
 *       
@@ -21,6 +23,13 @@
 *    - Number of jobs in each category (short, medium, long);
 *    - Average turnaround time and average normalized turnaround time, 
 *        within each category and for the system as a whole.
+
+    Important notes:
+    - Service time = an integer that represents the CPU run time required – provided as input data
+    - Arrival time = an integer that represents the time the process entered the system.
+                        To simplify your program assume all processes arrive at t=0.
+    - CPU Time = An integer that represents the amount of CPU run time received so far.
+                    Initially set to 0, but recomputed each time a process “executes”
 */
 
 
@@ -29,7 +38,7 @@
 Function: calcWaitTime
 --
 Arguments: int processes[], int counter, int burstTime[],
-            int waitTime[], int quantum
+            int waitTime[], int q
 --
 Return type: None (void)
             
@@ -38,7 +47,7 @@ This function will calculate the
 waiting time for all given processes
 ====================================================
 */
-void calcWaitTime(int processes[], int counter, int burstTime[], int waitTime[], int quantum)
+void calcWaitTime(int processes[], int counter, int burstTime[], int waitTime[], int q)
 {
 
     // we want something to store the burst times not yet used
@@ -52,44 +61,45 @@ void calcWaitTime(int processes[], int counter, int burstTime[], int waitTime[],
     // clocks at 0 initialization
     int e = 0;
 
-    while (true)
-    {
-        bool fin = true;
+ while (true){
 
-        // iterate through the processes
-        for (int i = 0; i < counter; i++)
-        {
-            if (remaining_burstTime > 0)
-            {
-                fin = false;
+        bool done = true;
 
-                if (remaining_burstTime[i] > quantum)
-                {
-                    e += quantum;
+        // iterate through all processes one by one
+        for (int i = 0; i < counter; i++){
+            if (remaining_burstTime[i] > 0){
+                done = false; 
 
-                    // now decrement the current process by quantum time
-                    remaining_burstTime[i] -= quantum;
+                if (remaining_burstTime[i] > q){
+                    // increment e by quantum
+                    e += q;
+                    // decriment process by quantum
+                    remaining_burstTime[i] -= q;
                 }
-                else
-                {
-                    // Show how much time a process has been taken
+
+                else{
+                    
+                    // e is increased to show the time it has been processing
                     e = e + remaining_burstTime[i];
 
-                    // wait time is measure by current time - time process is going
-                    waitTime[i] = e - burstTime[i];
+                    // waiting time formula
+                    waitTime[i] = e - remaining_burstTime[i];
 
-                    // burst time should be 0 if you get to this point
-                    // to show that the processes are fully executed
+                    // burstTime should be zero after process
+                    // is completely finished executing
                     remaining_burstTime[i] = 0;
                 }
             }
-            if (fin == true)
-            {
-                break;
-            }
+        }
+
+        // If all processes are done
+        if (done == true){
+            break;
+
         }
     }
 }
+
 
 /*
 ====================================================
@@ -118,7 +128,7 @@ void calcTAT(int processes[],int counter,int burstTime[],int waitTime[],int TAT[
 ====================================================
 Function: calcAWT
 --
-Arguments: int processes[],int counter,int burstTime[],int quantum
+Arguments: int processes[],int counter,int burstTime[],int q
 --
 Return type: None (void)
             
@@ -127,12 +137,12 @@ This function will calculate the
 Turnaround Time
 ====================================================
 */
-void calcAWT(int processes[],int counter,int burstTime[],int quantum){
+void calcAWT(int processes[],int counter,int burstTime[],int q){
     int waitTime[counter], TAT[counter];
     int totalWait = 0, totalTAT = 0;
 
     // call function for the waiting time
-    calcWaitTime(processes, counter, burstTime, waitTime, quantum);
+    calcWaitTime(processes, counter, burstTime, waitTime, q);
 
     // call function for the for Turnaround Time
     calcTAT(processes, counter, burstTime, waitTime, TAT);
@@ -149,8 +159,8 @@ void calcAWT(int processes[],int counter,int burstTime[],int quantum){
 
     }
     // display the average times
-    std::cout << "Average Waiting Time: " << (float)totalWait / (float)counter;
-    std::cout << "Average Turnaround Time: " << (float)totalTAT / (float)counter;
+    std::cout << "Average Waiting Time: " << (float)totalWait / (float)counter << std::endl;
+    std::cout << "Average Turnaround Time: " << (float)totalTAT / (float)counter << std::endl;
 
 }
 
@@ -167,7 +177,7 @@ This function will provide a reference
 to an integer array gathered from the input file
 ====================================================
 */
-int* getExec(std::string filename, int n){
+int* getExec(std::string filename,int n){
 
     /*
     Read in the file and assign the 
@@ -175,16 +185,49 @@ int* getExec(std::string filename, int n){
     as indexes of array to use in calculations 
     in main
     */
-    
-    // open a file and parse to get correct values
-    // to throw into this array pointer to return
+
+
+    // variable to check for ints in string
     int* CPUarr = new int[n]; 
-  
+    int qchecker;
+    int stchecker;
+    std::stringstream strs;
+    std::string line1, line2;
+    std::string temp;
+    std::ifstream infile (filename);
+
+    if(infile.is_open()){
+        std::cout << "\n\n=====================================================" << "\n";
+        std::cout << "Information given from text: " << "\n";
+        std::getline(infile, line1);
+        std::cout << line1 << std::endl;
+        std::getline(infile, line2);
+        std::cout << line2 << std::endl;
+
+        for (int i = 0; i < line1.length(); i++){
+            if(isdigit(line1[i])){
+                std::cout<< line1[i] << std::endl;
+
+            }
+        }
+           
+        std::cout << "\n=====================================================" << "\n\n";
+
+    }
+
+    // creating the stringstream object to hold our line
+
+    // make a dynamic array to be added to
+    // via values from the file
+
+    // variable to pull out integers from line
+
+
     // Some operations on arr[]
     // values of the array get assigned here
-    CPUarr[0] = 10; 
-    CPUarr[1] = 20; 
-  
+    CPUarr[1] = 10; 
+    CPUarr[2] = 50;
+
     return CPUarr; 
 }
 /*
@@ -198,25 +241,25 @@ Return Type: None (0)
 To provided a driver for the functions above
 ====================================================
 */
+
 int main(){
+
     int processes[] = {1,2,3};
     int counter = sizeof processes / sizeof processes[0];
 
     // burst time initilization
     int burstTime[] = {10, 5, 8};
 
-    // quantum time initilization
-    int quantum = 2;
+    // q time initilization
+    int q = 2;
+        
+    int n = 2;
     
-    /*
-    using the array above:
-    int n = 21;
-    
-    int* ptr = fun(n); 
-    std::cout << ptr[0] << " " << ptr[1]; 
-    */
+    int* ptr = getExec("sample.txt", n); 
+    std::cout << ptr[0] << " " << ptr[1] << " " << ptr[2] << std::endl; 
 
-    calcAWT(processes, counter, burstTime, quantum);
+    calcAWT(processes, counter, burstTime, q);
 
     return 0;
 }
+
