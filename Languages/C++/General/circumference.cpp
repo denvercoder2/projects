7db60ec6f3c532856cb87651f2a22536c022e936
River Holley <riverholley0923@gmail.com>
2019-11-25 10:52:40 -0600

    Added sample program for programming pals

--- Languages/C++/General/circumference.cpp
+++ Languages/C++/General/circumference.cpp
@@ -3,16 +3,39 @@ Examples of things in c++ for christian
 */
 
 #include <iostream>
-using namespace std;
-const double pi = 3.1415;
 
-int main(){
-    // radius
-    double r = 10.0; 
-    double circle;
-    circle = 2 * pi * r;
-    cout << circle;
+/*
+*   Function Name: getCircumference(double)
+*   Parameter Type: double
+*   Return Type: double
+*   Purpose: Compute the circumference, given the radius
+*/
 
-
-    return circle;
+double getCircumference(double radius){
+    const double PI = 3.14159;
+    double circumference = 2 * PI * radius;
+    return circumference;
 }
+
+/*
+*    Function: Main
+*    Parameter type: None (Void)
+*    Return Type: int (exit condition)
+*    Purpose: Driver for functions above
+*/
+int main(void){
+    // declare a variable to use in the function call
+    double radius = 2.00;
+
+    // call the function and assign it to the data type that
+    // is declared above. Now the value is stored in the variable
+    // circumference
+    double circumference = getCircumference(radius);
+
+    std::cout << "Circumference for given radius: " << radius << " is: " <<
+    circumference << std::endl;
+
+    return 0;
+}
+
+
