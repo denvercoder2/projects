f60900546d0388c16a4cbf95c9dbbce44c11d9ce
Scott Holley <denvercoder2@gmail.com>
2019-12-22 20:17:10 -0600

    Work on the interpreter

--- Icarus/Python/Parser.py
+++ Icarus/Python/Parser.py
@@ -1,7 +1,26 @@
-''' Icarus Parser '''
+def string_with_arrows(text, pos_start, pos_end):
+    result = ''
 
-'''
-Grammar Definitions
-- Term is a Factor (Multiplied or Divided) by another factor
-- An Expression is a Term (Plus or Minus) another term
-'''
+    # Calculate indices
+    idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
+    idx_end = text.find('\n', idx_start + 1)
+    if idx_end < 0: idx_end = len(text)
+    
+    # Generate each line
+    line_count = pos_end.ln - pos_start.ln + 1
+    for i in range(line_count):
+        # Calculate line columns
+        line = text[idx_start:idx_end]
+        col_start = pos_start.col if i == 0 else 0
+        col_end = pos_end.col if i == line_count - 1 else len(line) - 1
+
+        # Append to result
+        result += line + '\n'
+        result += ' ' * col_start + '^' * (col_end - col_start)
+
+        # Re-calculate indices
+        idx_start = idx_end
+        idx_end = text.find('\n', idx_start + 1)
+        if idx_end < 0: idx_end = len(text)
+
+    return result.replace('\t', '')
--- Icarus/Python/shell.py
+++ Icarus/Python/shell.py
@@ -1,12 +1,8 @@
-# runner
 import LexicalAnalyzer
 
-
 while True:
-    text = input("Icarus >>> ")
-    result, error = LexicalAnalyzer.run('<stdin>', text)
+		text = input('Icarus >>> ')
+		result, error = LexicalAnalyzer.run('<stdin>', text)
 
-    if error:
-        print(error.as_string())
-    else:
-        print(result)
+		if error: print(error.as_string())
+		else: print(result)
