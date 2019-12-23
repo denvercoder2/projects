0109101ad2d1370a26661056223254a40440ae85
Scott Holley <denvercoder2@gmail.com>
2019-12-22 22:57:02 -0600

    Work on interpreter

--- Icarus/Python/LexicalAnalyzer.py
+++ Icarus/Python/LexicalAnalyzer.py
@@ -114,33 +114,33 @@ class Lexer:
 		tokens = []
 
 		while self.current_char != None:
-				if self.current_char in ' \t':
-						self.advance()
-				elif self.current_char in DIGITS:
-						tokens.append(self.make_number())
-				elif self.current_char == '+':
-						tokens.append(Token(TT_PLUS, pos_start=self.pos))
-						self.advance()
-				elif self.current_char == '-':
-						tokens.append(Token(TT_MINUS, pos_start=self.pos))
-						self.advance()
-				elif self.current_char == '*':
-						tokens.append(Token(TT_MUL, pos_start=self.pos))
-						self.advance()
-				elif self.current_char == '/':
-						tokens.append(Token(TT_DIV, pos_start=self.pos))
-						self.advance()
-				elif self.current_char == '(':
-						tokens.append(Token(TT_LPAREN, pos_start=self.pos))
-						self.advance()
-				elif self.current_char == ')':
-						tokens.append(Token(TT_RPAREN, pos_start=self.pos))
-						self.advance()
-				else:
-						pos_start = self.pos.copy()
-						char = self.current_char
-						self.advance()
-						return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+			if self.current_char in ' \t':
+					self.advance()
+			elif self.current_char in DIGITS:
+					tokens.append(self.make_number())
+			elif self.current_char == '+':
+					tokens.append(Token(TT_PLUS, pos_start=self.pos))
+					self.advance()
+			elif self.current_char == '-':
+					tokens.append(Token(TT_MINUS, pos_start=self.pos))
+					self.advance()
+			elif self.current_char == '*':
+					tokens.append(Token(TT_MUL, pos_start=self.pos))
+					self.advance()
+			elif self.current_char == '/':
+					tokens.append(Token(TT_DIV, pos_start=self.pos))
+					self.advance()
+			elif self.current_char == '(':
+					tokens.append(Token(TT_LPAREN, pos_start=self.pos))
+					self.advance()
+			elif self.current_char == ')':
+					tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+					self.advance()
+			else:
+					pos_start = self.pos.copy()
+					char = self.current_char
+					self.advance()
+					return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
 		tokens.append(Token(TT_EOF, pos_start=self.pos))
 		return tokens, None
@@ -151,18 +151,18 @@ class Lexer:
 		pos_start = self.pos.copy()
 
 		while self.current_char != None and self.current_char in DIGITS + '.':
-				if self.current_char == '.':
-						if dot_count == 1: break
-						dot_count += 1
-						num_str += '.'
-				else:
-						num_str += self.current_char
-				self.advance()
+			if self.current_char == '.':
+					if dot_count == 1: break
+					dot_count += 1
+					num_str += '.'
+			else:
+					num_str += self.current_char
+			self.advance()
 
 		if dot_count == 0:
-				return Token(TT_INT, int(num_str), pos_start, self.pos)
+			return Token(TT_INT, int(num_str), pos_start, self.pos)
 		else:
-				return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+			return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
 
 #######################################
 # NODES
