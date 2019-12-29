ef3b5a2c626f030fd62f7fa966de40b6ae36a768
Scott Holley <denvercoder2@gmail.com>
2019-12-29 11:54:31 -0600

    Added work on Analyzer

--- Icarus/Python/shell.py
+++ Icarus/Python/shell.py
@@ -1,7 +1,7 @@
 import LexicalAnalyzer
 
 while True:
-		text = input('Icarus >>> ')
+		text = input('LexicalAnalyzer > ')
 		result, error = LexicalAnalyzer.run('<stdin>', text)
 
 		if error: print(error.as_string())
