db12d0dda843977d97703db2ce712671d0fa7968
Scott Holley <denvercoder2@gmail.com>
2019-12-29 12:46:23 -0600

    Changed the name of the interpreter

--- Icarus/Python/shell.py
+++ Icarus/Python/shell.py
@@ -1,7 +1,7 @@
 import LexicalAnalyzer
 
 while True:
-		text = input('LexicalAnalyzer > ')
+		text = input('Icarus >>> ')
 		result, error = LexicalAnalyzer.run('<stdin>', text)
 
 		if error: print(error.as_string())
