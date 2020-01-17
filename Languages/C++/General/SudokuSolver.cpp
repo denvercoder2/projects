bf67ff9d100d8f4905795f244f982876d5572c43
denvercoder2 <riverholley0923@gmail.com>
2020-01-16 22:46:54 -0600

    Restructured the programs and added a suduku solver template to work on this weekend

---
+++ Languages/C++/General/SudokuSolver.cpp
@@ -0,0 +1,24 @@
+/* 
+* Soduku Solver written in C++
+* Scott Holley
+* January 16th, 2016
+* 
+*/
+
+
+#include<stdlib.h>
+#include<vector>
+#include<iostream>
+
+
+/*
+*   Function: createMatrix
+*   Parameters: None
+*   Purpose: Create matrix of given size 
+*/
+
+int createMatrix(){
+    //TODO: Fix this in the near future.
+    std::vector <std::vector <int> > Mat(6, std::vector<int>(6, 0));
+
+}
